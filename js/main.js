934d38acc1d12dba757a837103cc3d0ad1bba054
gotlisch <mgotlib@gmail.com>
2015-09-03 09:31:00 +1000

    updates

--- js/main.js
+++ js/main.js
@@ -21,16 +21,18 @@ var app = {
 	},
 	
 	
-	renderHomeView: function() {
-    var html =
-            "<div class='header'><h1>Home</h1></div>" +
-            "<div class='search-view'>" +
-            "<input class='search-key'/>" +
-            "<ul class='employee-list'></ul>" +
-            "</div>"
-    $('body').html(html);
+renderHomeView: function() {
+    $('body').html(this.homeTpl());
     $('.search-key').on('keyup', $.proxy(this.findByName, this));
-	},
+},
+
+
+findByName: function() {
+    var self = this;
+    this.store.findByName($('.search-key').val(), function(employees) {
+        $('.employee-list').html(self.employeeLiTpl(employees));
+    });
+},
 
 };
 
