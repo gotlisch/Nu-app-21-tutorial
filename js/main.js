59d8e78e2082ded1693558a921a9c6130fcca839
gotlisch <mgotlib@gmail.com>
2015-09-03 09:48:09 +1000

    update

--- js/main.js
+++ js/main.js
@@ -13,24 +13,10 @@ var app = {
         });
     },
 
-	  initialize: function() {
-		var self = this;
-		this.store = new MemoryStore(function() {
-			self.renderHomeView();
-		});
-	},
-	
-	
-renderHomeView: function() {
-    $('body').html(this.homeTpl());
-    $('.search-key').on('keyup', $.proxy(this.findByName, this));
-},
-
-
-findByName: function() {
+	initialize: function() {
     var self = this;
-    this.store.findByName($('.search-key').val(), function(employees) {
-        $('.employee-list').html(self.employeeLiTpl(employees));
+    this.store = new MemoryStore(function() {
+        $('body').html(new HomeView(self.store).render().el);
     });
 },
 
